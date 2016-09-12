7711546caa540f2cc22c737bdc506f7e9e5b7563
Ilya Lyubimov <villytiger@gmail.com>
2016-09-13 00:51:46 +0300

    Initial commit

---
+++ include/vt/scope-exit.h
@@ -0,0 +1,78 @@
+
+//          Copyright Ilya Lyubimov 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <functional>
+#include <utility>
+
+#ifndef VT_SCOPE_GUARD_ON_STD_EXCEPTION
+#include <iostream>
+#define VT_SCOPE_GUARD_ON_STD_EXCEPTION(exception) \
+	do { \
+		std::cerr << e.what() << std::endl; \
+	} while (false)
+#endif // VT_SCOPE_GUARD_ON_STD_EXCEPTION
+
+#ifndef VT_SCOPE_GUARD_ON_ANY_EXCEPTION
+#include <iostream>
+#define VT_SCOPE_GUARD_ON_ANY_EXCEPTION() \
+	do { \
+		std::cerr << "An error occured in scope guard" << std::endl; \
+	} while (false)
+#endif // VT_SCOPE_GUARD_ON_ANY_EXCEPTION
+
+#ifndef VT_SCOPE_GUARD_WRAP_ACTION
+#include <exception>
+#define VT_SCOPE_GUARD_WRAP_ACTION(action) \
+	do { \
+		try { \
+			action(); \
+		} catch (const std::exception& e) { \
+			try { \
+				VT_SCOPE_GUARD_ON_STD_EXCEPTION(e); \
+			} catch (...) { \
+			} \
+		} catch (...) { \
+			try { \
+				VT_SCOPE_GUARD_ON_ANY_EXCEPTION(); \
+			} catch (...) { \
+			} \
+		} \
+	} while (false)
+#endif // VT_SCOPE_GUARD_WRAP_ACTION
+
+#define VT_SCOPE_GUARD_DO_STRING_JOIN(arg1, arg2) arg1 ## arg2
+#define VT_SCOPE_GUARD_STRING_JOIN(arg1, arg2) VT_SCOPE_GUARD_DO_STRING_JOIN(arg1, arg2)
+
+#define SCOPE_EXIT(...) \
+	vt::scopeGuard::ScopeExit VT_SCOPE_GUARD_STRING_JOIN(scopeExit, __COUNTER__) = [__VA_ARGS__]()
+
+namespace vt {
+namespace scopeGuard {
+
+class ScopeExit {
+public:
+	ScopeExit(ScopeExit&) = delete;
+	ScopeExit& operator=(ScopeExit&) = delete;
+	ScopeExit(ScopeExit&& other) = default;
+	ScopeExit& operator=(ScopeExit&&) = default;
+
+	template<typename T>
+	ScopeExit(T&& action)
+		: mAction(std::forward<T>(action))
+	{}
+
+	~ScopeExit() {
+		VT_SCOPE_GUARD_WRAP_ACTION(mAction);
+	}
+
+private:
+	std::function<void()> mAction;
+};
+
+}
+}
